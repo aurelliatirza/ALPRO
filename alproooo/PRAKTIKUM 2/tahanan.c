c249fa8a8cee3bf5f4a4bc1e8004eba6dc33a832
aurelliatirza <126539060+aurelliatirza@users.noreply.github.com>
2023-03-07 16:18:27 +0700

    Update tahanan.c

--- alproooo/PRAKTIKUM 2/tahanan.c
+++ alproooo/PRAKTIKUM 2/tahanan.c
@@ -1,12 +1,17 @@
+//Nama: Tirza Aurellia Wijaya//
+//NIM: 24060122130047//
+//Hari: Senin, 6 Maret 2023//
+//Deskripsi: menentukan total nomor tahanan yang tidak negatif//
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void){
+// Kamus //
     int a;  //masukan nilai tahanan 1//
     int b;  //masukan nilai tahanan 2//
     int c;  //masukan nilai tahanan 3//
     int x;  //hasil operasi penjumlahan 3 tahanan//
-
+// Algoritma //
     printf ("masukkan nilai tahanan 1: ");
     scanf ("%d", &a);
 
