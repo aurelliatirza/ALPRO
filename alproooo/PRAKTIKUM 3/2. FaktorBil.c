123aacc5fc2bf2a18b8f7baa33694942296e9da0
aurelliatirza <126539060+aurelliatirza@users.noreply.github.com>
2023-03-14 21:01:08 +0700

    Update 2. FaktorBil.c

--- alproooo/PRAKTIKUM 3/2. FaktorBil.c
+++ alproooo/PRAKTIKUM 3/2. FaktorBil.c
@@ -5,7 +5,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main(){
+int main(void){
 //Kamus//
    int N,i;
 
@@ -19,4 +19,5 @@ int main(){
             printf("%d ", i);
         }
     }
+ return 0;
 }
